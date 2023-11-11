bd5165d47558597f41718263666cbdad1b3a537f
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2023-11-11 13:28:57 +0530

    Delete 2023-June_VersionB/June2023OOPDesignPatternQuestion directory

--- 2023-June_VersionB/June2023OOPDesignPatternQuestion/MissileSystem.java
+++
@@ -1,6 +0,0 @@
-package June2023OOPDesignPatternQuestion;
-
-public interface MissileSystem {
-	public void launch(String source);
-	public void blast(String destination);
-}
--- 2023-June_VersionB/June2023OOPDesignPatternQuestion/Test.java
+++
@@ -1,7 +0,0 @@
-package June2023OOPDesignPatternQuestion;
-
-public class Test {
-	public static void main(String[] args) {
-		
-	}
-}
